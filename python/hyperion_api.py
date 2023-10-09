38d745c60957fba071adffb0193673d356ee3179
koumouz <constantin@fluentdynamics.ai>
2023-10-09 09:42:05 -0700

    - Update python version

--- python/hyperion_api.py
+++ python/hyperion_api.py
@@ -22,16 +22,94 @@ class HyperionAPI:
         }
         params.update(kwargs)
 
+        if HyperionAPI.protocol == "REST":
+            api_method = "mode/generate/"
+        else:
+            api_method = "model.generate"
+
+        return HyperionAPI._call(api_method, params, stream)
+
+    def load_model(model_name, stream=False):
+        params = {
+            "model_name": model_name,
+            "stream": stream,
+        }
         if HyperionAPI.mode == "REST":
-            request = {"params": params}
-            response = requests.post(
-                f"http://{HyperionAPI.uri}/model/generate/", json=request
-            )
-            response = response.json()
+            api_method = "model/load"
+        else:
+            api_method = "model.load"
+
+        return HyperionAPI._call(api_method, params, stream)
+
+    def unload_model(model_name):
+        params = {"model_name": model_name}
+        if HyperionAPI.mode == "REST":
+            api_method = "model/unload/"
+        else:
+            api_method = "model.unload"
+
+        return HyperionAPI._call(api_method, params)
+
+    def get_loaded_models():
+        params = None
+        if HyperionAPI.protocol == "REST":
+            api_method = "model/loaded/"
+        else:
+            api_method = "model.get_loaded"
+
+        return HyperionAPI._call(api_method, params)
+
+    def get_cached_models():
+        params = None
+        if HyperionAPI.protocol == "REST":
+            api_method = "model/cached/"
+        else:
+            api_method = "model.get_cached"
+
+        return HyperionAPI._call(api_method, params)
+
+    def tokenize(model_name, text):
+        params = {
+            "model_name": model_name,
+            "text": text,
+        }
+
+        if HyperionAPI.protocol == "REST":
+            api_method = "model/tokenize/"
+        else:
+            api_method = "model.tokenize"
+
+        return HyperionAPI._call(api_method, params)
+
+    def detokenize(model_name, tokens):
+        params = {
+            "model_name": model_name,
+            "tokens": tokens,
+        }
+        if HyperionAPI.protocol == "REST":
+            api_method = "model/detokenize/"
+        else:
+            api_method = "model.detokenize"
+
+        return HyperionAPI._call(api_method, params)
+
+    def _call(api_method, params=None, stream=False):
+        if HyperionAPI.protocol == "REST":
+            if params is None:
+                response = requests.get(
+                    f"http://{HyperionAPI.uri}/{api_method}", json=request
+                )
+                response = response.json()
+            else:
+                request = {"params": params}
+                response = requests.post(
+                    f"http://{HyperionAPI.uri}/{api_method}", json=request
+                )
+                response = response.json()
             return response
-        elif HyperionAPI.mode == "WEBSOCKET":
+        elif HyperionAPI.protocol == "WEBSOCKET":
             websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.generate", "params": params}
+            request = {"method": f"{api_method}", "params": params}
             websocket.send(json.dumps(request))
 
             if stream == True and HyperionAPI.stream_callback is not None:
@@ -44,135 +122,6 @@ class HyperionAPI:
             # Get the final complete output
             response = websocket.recv()
             response = json.loads(response)
-            websocket.close()
-            return response
-        else:
-            return {"success": "false", "error": "Unknown server mode"}
-
-    def load_model(model_name, stream=False):
-        params = {
-            "model_name": model_name,
-            "stream": stream,
-        }
-        if HyperionAPI.mode == "REST":
-            request = {"params": params}
-            response = requests.post(
-                f"http://{HyperionAPI.uri}/model/load/", json=request
-            )
-            response = response.json()
-            return response
-        elif HyperionAPI.mode == "WEBSOCKET":
-            websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.load", "params": params}
-            websocket.send(json.dumps(request))
-
-            if stream == True and HyperionAPI.stream_callback is not None:
-                while True:
-                    response = json.loads(websocket.recv())
-                    HyperionAPI.stream_callback(response)
-                    if response.get("stream_end") == True:
-                        break
-
-            response = json.loads(websocket.recv())
-            websocket.close()
-            return response
-        else:
-            return {"success": "false", "error": "Unknown server mode"}
-
-    def unload_model(model_name):
-        params = {"model_name": model_name}
-        if HyperionAPI.mode == "REST":
-            request = {"params": params}
-            response = requests.post(
-                f"http://{HyperionAPI.uri}/model/unload/", json=request
-            )
-            response = response.json()
-            return response
-        elif HyperionAPI.mode == "WEBSOCKET":
-            websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.unload", "params": params}
-            websocket.send(json.dumps(request))
-            response = json.loads(websocket.recv())
-            websocket.close()
-            return response
-        else:
-            return {"success": "false", "error": "Unknown server mode"}
-
-    def get_loaded_models():
-        params = {}
-        if HyperionAPI.mode == "REST":
-            response = requests.get(
-                f"http://{HyperionAPI.uri}/model/loaded/", json=request
-            )
-            response = response.json()
-            return response
-        elif HyperionAPI.mode == "WEBSOCKET":
-            websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.get_loaded"}
-            websocket.send(json.dumps(request))
-            response = json.loads(websocket.recv())
-            websocket.close()
-            return response
-        else:
-            return {"success": "false", "error": "Unknown server mode"}
-
-    def get_cached_models():
-        params = {}
-        if HyperionAPI.mode == "REST":
-            response = requests.get(
-                f"http://{HyperionAPI.uri}/model/cached/", json=request
-            )
-            response = response.json()
-            return response
-        elif HyperionAPI.mode == "WEBSOCKET":
-            websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.get_cached"}
-            websocket.send(json.dumps(request))
-            response = json.loads(websocket.recv())
-            websocket.close()
-            return response
-        else:
-            return {"success": "false", "error": "Unknown server mode"}
-
-    def tokenize(model_name, text):
-        params = {
-            "model_name": model_name,
-            "text": text,
-        }
-        if HyperionAPI.mode == "REST":
-            request = {"params": params}
-            response = requests.post(
-                f"http://{HyperionAPI.uri}/model/tokenize/", json=request
-            )
-            response = response.json()
-            return response
-        elif HyperionAPI.mode == "WEBSOCKET":
-            websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.tokenize", "params": params}
-            websocket.send(json.dumps(request))
-            response = json.loads(websocket.recv())
-            websocket.close()
-            return response
-        else:
-            return {"success": "false", "error": "Unknown server mode"}
-
-    def detokenize(model_name, tokens):
-        params = {
-            "model_name": model_name,
-            "tokens": tokens,
-        }
-        if HyperionAPI.mode == "REST":
-            request = {"params": params}
-            response = requests.post(
-                f"http://{HyperionAPI.uri}/model/detokenize/", json=request
-            )
-            response = response.json()
-            return response
-        elif HyperionAPI.mode == "WEBSOCKET":
-            websocket = create_connection(f"ws://{HyperionAPI.uri}")
-            request = {"method": f"model.detokenize", "params": params}
-            websocket.send(json.dumps(request))
-            response = json.loads(websocket.recv())
             websocket.close()
             return response
         else:
